a5529dd80a7f6b5f83a24b4232e1ca1b331da764
chikaki <r.chikaki@gmail.com>
2019-03-12 17:51:33 +0900

    wip

--- models/base/snads/snads_proc.sql
+++ models/base/snads/snads_proc.sql
@@ -7,5 +7,5 @@ from (
 select
 *,
 first_value(time_of_entry ) over (partition by date, campaign order by time_of_entry desc) lv
-from `{{ target.project }}.agency_data_pipeline.asa` 
+from `{{ target.project }}.agency_data_pipeline.snads` 
 ) where time_of_entry = lv
